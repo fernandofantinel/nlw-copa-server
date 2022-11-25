cfbf34fcb582c0c5f5f6590ee19a72ec6fa343f4
Fernando Fantinel <fantinel89@gmail.com>
2022-11-25 10:04:05 -0300

    First commit

---
+++ prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'john.doe@gmail.com',
+      avatarUrl: 'https://github.com/fernandofantinel.png'
+    }
+  })
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: 'Example Pool',
+      code: 'BOL001',
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id
+        }
+      }
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-24T16:00:00.798Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'RS'
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-26T13:00:00.627Z',
+      firstTeamCountryCode: 'FR',
+      secondTeamCountryCode: 'DK',
+
+      guesses: {
+        create: {
+          firstTeamPoints: 1,
+          secondTeamPoints: 0,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id
+              }
+            }
+          }
+        }
+      }
+    }
+  })
+}
+
+main()
